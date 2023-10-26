f78bf121468fdecf875eb7bc92ce31a0cf00d661
Leandro Francisco <leandro.francisco@enube.me>
2023-10-26 00:29:27 -0300

    feat(navbar): Completes the implementation of the navigation component (Navbar)

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Navbar } from "./components/Navbar"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <main>
+        <div id="home">
+
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
